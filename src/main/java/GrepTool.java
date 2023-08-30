d7756f410f7963f892885f5f3199000d19acfe69
vislower <115408772+vislower@users.noreply.github.com>
2023-08-30 10:31:58 +0200

    Add BufferedReader to read input of files and Set to store flags passed as argument

--- src/main/java/GrepTool.java
+++ src/main/java/GrepTool.java
@@ -1,11 +1,40 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class GrepTool {
 
     String grep(String pattern, List<String> flags, List<String> files) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Set<String> grepFlags = new HashSet<>(flags);
+        StringBuilder sb = new StringBuilder();
+
+        for (String file : files) {
+
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(new File(file)));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    if (grepFlags.contains("-i")) {
+                        line.toLowerCase();
+                    }
+                }
+
+
+                reader.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            sb.append("\n");
+        }
+
+        return sb.toString();
     }
 
 }
