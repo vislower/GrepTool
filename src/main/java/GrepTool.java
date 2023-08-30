b8b292da9b60edd288181d4365252261f0a461f8
vislower <115408772+vislower@users.noreply.github.com>
2023-08-30 10:07:19 +0200

    Initial state of the project

---
+++ src/main/java/GrepTool.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.util.List;
+
+class GrepTool {
+
+    String grep(String pattern, List<String> flags, List<String> files) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
