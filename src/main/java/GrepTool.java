04b8cd041f14c215ef42316855a76822004b391a
vislower <115408772+vislower@users.noreply.github.com>
2023-08-31 18:47:22 +0200

    New iteration with some cleanup and more concise code

--- src/main/java/GrepTool.java
+++ src/main/java/GrepTool.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,7 +12,7 @@ class GrepTool {
 
     String grep(String pattern, List<String> flags, List<String> files) {
         Set<String> grepFlags = new HashSet<>(flags);
-        StringBuilder sb = new StringBuilder();
+        Collection<CharSequence> matches = new ArrayDeque<>();
 
         for (String file : files) {
 
@@ -19,7 +21,7 @@ class GrepTool {
                 String line;
                 int lineNumber = 1;
                 while ((line = reader.readLine()) != null) {
-                    boolean match = matchPattern(pattern, sb, grepFlags, line, lineNumber, file, files.size() > 1);
+                    boolean match = matchPattern(pattern, matches, grepFlags, line, lineNumber, file, files.size() > 1);
                     if (match && grepFlags.contains("-l")) { // don't print multiple times same file name
                         break;
                     }
@@ -33,91 +35,66 @@ class GrepTool {
 
         }
 
-        return sb.length() == 0 ? "" : sb.toString();
+        return String.join("\n", matches);
     }
 
-    boolean matchPattern(String patternToMatch, StringBuilder sb, Set<String> grepFlags, String line, int lineNumber, String fileName, boolean multipleFiles) {
+    boolean matchPattern(String patternToMatch, Collection<CharSequence> matches, Set<String> grepFlags, String line, int lineNumber, String fileName, boolean multipleFiles) {
         String lineBackup = line; // keep backup of original line
-        if (grepFlags.contains("-i")) {
+        if (grepFlags.contains("-i")) { // if search is case insensitive
             line = line.toLowerCase();
             patternToMatch = patternToMatch.toLowerCase();
         }
 
-        if (grepFlags.contains("-x")) {
+
+        if (grepFlags.contains("-x")) { // match entire line
             if (line.equals(patternToMatch) && !grepFlags.contains("-v")) {
-                if (!sb.isEmpty()) {
-                    sb.append("\n");
-                }
-                if (grepFlags.contains("-l")) {
-                    sb.append(fileName);
+                if (grepFlags.contains("-l")) { // only print file name
+                    matches.add(fileName);
                     return true;
                 }
-                
-                if (multipleFiles) {
-                    sb.append(fileName + ":");
-                }
-                if (grepFlags.contains("-n")) {
-                    sb.append(lineNumber + ":" + lineBackup);
-                } else {
-                    sb.append(lineBackup);
-                }
+                formatLine(multipleFiles, fileName, lineNumber, lineBackup, grepFlags, matches);
                 return true;
             } else if (grepFlags.contains("-v") && !line.equals(patternToMatch)) {
-                if (!sb.isEmpty()) {
-                    sb.append("\n");
-                }
-                if (multipleFiles) {
-                    sb.append(fileName + ":");
-                }
-                if (grepFlags.contains("-n")) {
-                    sb.append(lineNumber + ":" + lineBackup);
-                } else {
-                    sb.append(lineBackup);
-                }
+                formatLine(multipleFiles, fileName, lineNumber, lineBackup, grepFlags, matches);
                 return true;
             } else {
                 return false;
             }
         }
 
-        if (line.contains(patternToMatch)) {
+        if (line.contains(patternToMatch)) { // search a match in the line
             if (grepFlags.contains("-v")) {
                 return false;
             }
-            if (!sb.isEmpty()) {
-                sb.append("\n");
-            }
             if (grepFlags.contains("-l")) {
-                sb.append(fileName);
+                matches.add(fileName);
                 return true;
             }
-            if (multipleFiles) {
-                sb.append(fileName + ":");
-            }
-            if (grepFlags.contains("-n")) {
-                sb.append(lineNumber + ":" + lineBackup);
-            } else {
-                sb.append(lineBackup);
-            }
+            formatLine(multipleFiles, fileName, lineNumber, lineBackup, grepFlags, matches);
             return true;
         } else {
             if (grepFlags.contains("-v")) {
-                if (!sb.isEmpty()) {
-                    sb.append("\n");
-                }
-                if (multipleFiles) {
-                    sb.append(fileName + ":");
-                }
-                if (grepFlags.contains("-n")) {
-                    sb.append(lineNumber + ":" + lineBackup);
-                } else {
-                    sb.append(lineBackup);
-                }
+                formatLine(multipleFiles, fileName, lineNumber, lineBackup, grepFlags, matches);
                 return true;
             }
             return false;
         }
     
+    }
+
+    private void formatLine(boolean multipleFiles, String fileName, int lineNumber, String lineBackup, Set<String> grepFlags, Collection<CharSequence> matches) {
+        StringBuilder formatedLine = new StringBuilder();
+
+        if (multipleFiles) {
+            formatedLine.append(fileName + ":");
+        }
+        if (grepFlags.contains("-n")) {
+            formatedLine.append(lineNumber + ":" + lineBackup);
+        } else {
+            formatedLine.append(lineBackup);
+        }
+
+        matches.add(formatedLine);
     }
 
 }
